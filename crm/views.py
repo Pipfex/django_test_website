1956fdd0f07dd4e08c2bd632dc8afeb5495f234b
Pipfex <87333392+Pipfex@users.noreply.github.com>
2021-08-02 22:26:13 +0300

    forms

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Order
+from  .forms import OrderForms
 
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    form = OrderForms()
+    return render(request, './index.html', {'object_list': object_list,
+                                            'form':form})
+
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    elements = Order(order_name=name, order_phone=phone)
+    elements.save()
+    return render(request, './thanks_page.html', {'name': name,
+                                                  'phone': phone})
